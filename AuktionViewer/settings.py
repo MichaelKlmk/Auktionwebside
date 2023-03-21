ca72df84a18c7f87a974a191ee547aa34d53b16f
Michael Klimik <michael.klimik@gmail.com>
2023-03-21 11:13:52 +0100

    todos

---
+++ AuktionViewer/settings.py
@@ -0,0 +1,6 @@
+import os
+from pathlib import Path
+
+from django.contrib import messeges
+
+BASE_DIR = Path(__file__).resolve().parent.parent
---
+++ AuktionViewer/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include, reverse_lazy
+
+from AuktionViewer.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path()
+]
